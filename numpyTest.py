f97456a517d7a335113437a3950da04613abee9b
ldust667 <ldust667@gmail.com>
2019-08-14 16:13:31 +0000

    Zeros

--- numpyTest.py
+++ numpyTest.py
@@ -1,1 +1,7 @@
-Hello
+import numpy as np
+
+
+#zeros created an array with float values equal to the arguement
+b= np.zeros(3)
+print "np.zeros creates arrays with zero float values: " + str(b)
+print type(b[0])
