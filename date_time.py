4ea39f6378c5e1b4f32996dc819125e652ee591f
ldust667 <ldust667@gmail.com>
2020-02-17 13:18:39 -0500

    datetime_discover

---
+++ date_time.py
@@ -0,0 +1,9 @@
+import datetime
+#list out objects contained in datetime library
+#print(dir(datetime))
+#breaking it down further by explaining the class
+#help(datetime.date)
+#from the help we can see how to create a date object
+date_ex = datetime.date(1996, 5, 25)
+print(date_ex)
+
